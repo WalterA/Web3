163fb91694be0311b9623f5398e3146e38329c0c
WalterA <walteralb22@gmail.com>
2025-03-17 19:02:33 +0100

    OK

--- LezioneNode/webapp/src/LoginForm.js
+++ LezioneNode/webapp/src/LoginForm.js
@@ -1,78 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import {utenti} from './Login';
-
+import React, { useState } from 'react'
+import { utenti } from './Login';
 
 const LoginForm = () => {
-    let [username,setUser] = useState('');
-    let [password,setPass] = useState('');
-    let [visualizzaCambio , setVisualizzaCambio] = useState(false);
-    let [nuovo , setNuovo] = useState({})
-    let [nuovapass, setNuovapass] = useState("")
 
-    let bool=()=>{
+    let [mieutenze, setmieutenze] = useState(utenti);
+    let [esito,setesito] = useState("");
+    let [visualizzaCambio, setvisualizzaCambio] = useState(false);
+    let [strverifica, setstrverifica] = useState(false);
+    let [utenteLoggato, setutenteLoggato] = useState();
+
+    let verifica = () => {
+
+        let user = document.getElementById("user").value;
+        let pass = document.getElementById("pass").value;
         
-        let ok = "";
-        let alert = document.getElementById("alert");
-        let bool = false;
-    for (let utente of utenti) {
-        if (utente.username === username && utente.password === password) {
-            ok="«login success»"; 
-            setNuovo(utente);
-        
-            alert.innerText = ok;
-            alert.style.color = "green";
-            bool = true;
-                
-        }};
-        if (!bool){
-            ok="«login failed»";
-      
-            alert.innerText = ok;
-            alert.style.color = "red";
-        };
-    };
-    
-    let cambio=(el)=>{
-        setVisualizzaCambio(true);
-        let ok="";
-        let pass=document.getElementById("cambio");
-        setNuovo({
-            username: el.username,
-            
+        mieutenze.map( (el) => {
+            if(el.username == user && el.password == pass){
+                setstrverifica(true);
+                setutenteLoggato(el);
+            }
         })
 
-        
-
-
+        if(verifica){
+            setesito("TUTTO OK");
+        }else{
+            setesito("LOGIN ERRATA");
+        }
     }
 
-return (
-    <div>LoginForm
-        <div className='container'>
-            <div className='row'>
-                <div className='col-md-6 mt-5 mx-auto'>
-                    <input type='text' className='form-control' placeholder='UserName' value={username}  onChange={(e) => setUser(e.target.value)} />
-                    <input type='password' className='form-control' placeholder='Password'value={password}  onChange={(e) => setPass(e.target.value)}/>
-                    <button id="Btn" className='btn btn-lg btn-primary btn-block' onClick={bool}>Login</button>
-                    <button id="Btn" className='btn btn-lg btn-primary btn-block' onClick={cambio}>Cambio</button>
-                    <div id="alert" className="alert alert-primary" >Benvenuto</div>
-                    {visualizzaCambio && <div style={{ display: visualizzaCambio ? 'block' : ' none'}}>
-                    <button id="Btn" className='btn btn-lg btn-primary btn-block' onClick={cambio}>Conferma</button>
-                    <input type='password' className='form-control' placeholder='Password'value={nuovapass}  onChange={(e) => setPass(e.target.value)}/>
-                    <div id="cambio" className="alert alert-primary" ></div>
-                    </div>}
+    let cambio = () => {
+        setvisualizzaCambio(true);  
+    }
 
-                </div>
-            </div>
-        </div>
+    let cambiopwd = () => {
+        
+        let pass = document.getElementById("nuovapass").value;
 
+        let utenteModifica = {
+            id :utenteLoggato.id,
+            username : utenteLoggato.username,
+            password : pass
+        }
 
+        let nuovalista = mieutenze.filter( x => x.id != utenteModifica.id);
+        nuovalista.push(utenteModifica);
+        console.log("utenteModifica");
+        console.log(utenteModifica);
+        console.log(nuovalista);
+        for(let i of utenti){
+            if (i.password !== utenteModifica.password && i.id === utenteModifica.id){
+               return utenti.password= utenteModifica.password;
+            }
+            console.log(utenti);
+        }
+    }
 
-
-
-
+  return (
+    <div>
+        <h1>Esercizio4_9</h1>
+            UserName <input type="text" id="user"/><br/>
+            Password <input type="text" id="pass"/><br/>
+            <button onClick={ () => { verifica() }}>INVIA</button>
+            <button onClick={ () => { cambio() }}>CAMBIO PASSWORD</button>
+            <div> {esito} </div>
+            { strverifica && <div style={{ display : visualizzaCambio ? "block" : "none"}} >
+                <input type="text" id="nuovapass"/>
+                <button onClick={ () => { cambiopwd() }}>CONFERMA</button>
+            </div> }
     </div>
-  )};
-
+  )
+}
 
 export default LoginForm
